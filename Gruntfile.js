4b2ce48d1f21ac75d7a59c329c895e97bed67c1a
Alex <fedorov@mstek.ru>
2015-09-09 19:58:10 +0500

    add fest

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,13 +16,24 @@ module.exports = function (grunt) {
             },
             foo: 'bar' // For some reason an extra key with a non-object value is necessary
         },
+        fest:{
+            templates:{
+                files: [{
+                    expand:true,
+                    cwd: 'templates',
+                    src: '*.xml',
+                    dest:'public_html/js/tmpl'
+                }],
+            }
+        }
 
     });
 
     //grunt.loadNpmTasks('grunt-contrib-concat');
     //grunt.loadNpmTasks('grunt-sell');
     grunt.loadNpmTasks('grunt-web-server');
+    grunt.loadNpmTasks('grunt-fest');
 
-    grunt.registerTask('default', ['web_server']);
+    grunt.registerTask('default', ['web_server', 'fest:templates']);
 };
 
