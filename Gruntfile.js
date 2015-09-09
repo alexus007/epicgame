3da6c170bd674a6db5a5aa1243c16ab054a1f013
Alex <fedorov@mstek.ru>
2015-09-09 18:19:11 +0500

    grunt add

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,5 +4,14 @@
 
 module.exports = function (grunt) {
 
+    grunt.initConfig({
 
+        concat:{
+
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('default', ['concat']);
 };
